382959729e70b9f33bdfa0af4ceff024de30ebb0
patrickikhidero <80291797+patrickikhidero@users.noreply.github.com>
2022-01-31 09:03:38 +0100

    added-interactivity:runner:

---
+++ src/App.js
@@ -0,0 +1,102 @@
+ // import logo from './logo.svg';
+import './App.css';
+import Todo from "./components/Todo";
+import Form from "./components/Form";
+import FilterButton from './components/FilterButton';
+import React, {useState} from "react";
+import { nanoid } from "nanoid";
+
+
+const FILTER_MAP = {
+  All: () => true,
+  Active: task => !task.completed,
+  Completed: task => task.completed
+};
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
+
+function App(props) {
+  const [tasks, setTasks] = useState(props.tasks);
+  const [filter, setFilter] = useState('All');
+  
+  function toggleTaskCompleted(id){
+    const updatedTasks = tasks.map(task => {
+      if (id === task.id) {
+        return {...task, completed: !task.completed}
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
+  // editTask
+  function editTask(id, newName){
+    const editTaskList = tasks.map(task => {
+      if (id === task.id) {
+        return {...task, name: newName}
+      }
+      return task;
+    });
+    setTasks(editTaskList)
+  }
+//  deleteTask
+  function deleteTask(id){
+    const remainingTasks = tasks.filter(task => id !==task.id);
+    setTasks(remainingTasks);
+  }
+
+// addTask
+  function addTask(name) {
+    const newTask = {id: "todo-" + nanoid(), name: name, completed: false };
+    setTasks([...tasks, newTask]);
+  }
+  const taskList = tasks
+    .filter(FILTER_MAP[filter])
+    .map(task => (
+      <Todo
+        id={task.id}
+        name={task.name}
+        completed={task.completed}
+        key={task.id}
+        toggleTaskCompleted={toggleTaskCompleted}
+        deleteTask={deleteTask}
+        editTask={editTask}
+      />
+    ));
+
+
+  const filterList = FILTER_NAMES.map(name => (
+    <FilterButton 
+    key={name} 
+    name={name}
+    isPressed={name===filter}
+    setFilter={setFilter}
+    />
+  ));
+  
+  const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
+  const headingText = `${taskList.length} ${tasksNoun} remaining`;
+  return (
+    <div className="todoapp stack-large">
+      <h1>DoAm</h1>
+      <Form addTask={addTask} />
+
+      <div className="filters btn-group stack-exception">
+        {filterList}
+      </div>
+      <h2 id="list-heading">
+        {headingText}
+      </h2>
+        <ul
+            role="list"
+            className="todo-list stack-large stack-exception"
+            aria-labelledby="list-heading"
+        >
+          {taskList}
+        </ul>
+
+    </div>
+  );
+}
+
+
+export default App;
